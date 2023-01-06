d250e437d0cf13f41ec7a8f2aeaa6ef649559a4f
frank-Mwangi <kungofrank@gmail.com>
2023-01-06 15:55:38 -0800

    Arguments counter

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,17 @@
 if __name__ == "__main__":
     import sys
 
-    n = (len(sys.argv)) - 1
+    n = len(sys.argv) - 1
     suffix = 's'
     separator = ':'
 
     if n == 0:
         separator = '.'
+
     if n == 1:
         suffix = ''
+
     print("{} argument{}{}".format(n, suffix, separator))
-    for i in range(1, n):
+    
+    for i in range(1, n + 1):
         print("{}: {}".format(i, sys.argv[i]))
