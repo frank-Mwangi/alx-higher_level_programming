8e313819b245a0d5aa17f34c257eb07549721c2e
frank-Mwangi <kungofrank@gmail.com>
2023-01-06 15:39:03 -0800

    Arguments counter

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,15 @@ def args(argv):
     if n == 0:
         print("{:d} argument.".format(n))
         return
-    elif n == 1:
-        print("{:d} argument:".format(n))
     else:
-        print("{:d} arguments:".format(n))
-    i = 1
-    while 1 <= n:
-        print("{:d}: {:s}".format(i, argv[i]))
-        i += 1
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while 1 <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
 
 
 if __name__ == "__main__":
