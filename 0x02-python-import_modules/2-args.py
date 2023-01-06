645002e6f513ae85204819f89747213d36851cca
frank-Mwangi <kungofrank@gmail.com>
2023-01-06 15:36:14 -0800

    Argument counter

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,18 @@
 def args(argv):
     n = len(argv) - 1
     if n == 0:
-        print("{} argument.".format(n))
+        print("{:d} argument.".format(n))
         return
     elif n == 1:
-        print("{} argument:".format(n))
+        print("{:d} argument:".format(n))
     else:
-        print("{} arguments:".format(n))
+        print("{:d} arguments:".format(n))
     i = 1
-    while 1 < n:
-        print("{}: {}".format(i, argv[i]))
+    while 1 <= n:
+        print("{:d}: {:s}".format(i, argv[i]))
         i += 1
+
+
 if __name__ == "__main__":
     import sys
     args(sys.argv)
