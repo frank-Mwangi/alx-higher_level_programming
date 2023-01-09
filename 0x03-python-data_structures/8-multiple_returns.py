5b5ad9837ca8fb1555bb0c37f6b39d2c1eca79d9
frank-Mwangi <kungofrank@gmail.com>
2023-01-09 07:38:18 -0800

    Creating a tuple froma string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if len(sentence) == 0:
         sentence[0] = None
-    answer = (len(sentence), sentence[0])
+    answer = len(sentence), sentence[0]
     return (answer)
