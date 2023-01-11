2dfe38c24c55d43daade13f1f08fc6835df75032
frank-Mwangi <kungofrank@gmail.com>
2023-01-11 11:01:54 -0800

    Print dictionary in sorted key order

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    keys = list(a_dictionary.keys())
-    keys.sort()
-    result = {i: a_dictionary[i] for i in keys}
-    for j in result:
-        print("{} : {}".format(j, result[j]))
+    for k, v in sorted(a_dictionary.items()):
+        print("{} : {}".format(k, v))
