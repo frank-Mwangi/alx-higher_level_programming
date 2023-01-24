72358248b67bebd72da4c850a437ebbde1c28035
frank-Mwangi <kungofrank@gmail.com>
2023-01-24 10:19:58 -0800

    Printing a square on stdout

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,7 +28,10 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        for i in range(0, self.__size):
-            for j in range(0, self.__size):
-                print("#", end="")
-            print()
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
