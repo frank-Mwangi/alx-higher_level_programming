d403f1094a679b41159874a0c16d7527cd941cf0
frank-Mwangi <kungofrank@gmail.com>
2023-02-06 07:09:04 -0800

    Check if object is instance of a calass

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Checks if object is instance of specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if object is instance of a class"""
+
+    if isinstance(obj, a_class):
+        return True
+    return False
