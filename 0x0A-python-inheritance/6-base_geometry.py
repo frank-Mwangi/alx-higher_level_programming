be97eaf1826f68a16c8ab4dc7f0a03bd74a5941d
frank-Mwangi <kungofrank@gmail.com>
2023-02-06 10:59:54 -0800

    Adding an emty method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing empty class
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    pass
+    def area(self):
+        """Undefined method"""
+        raise Exception("area() is not implemented")
