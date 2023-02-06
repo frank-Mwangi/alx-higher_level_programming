30a9af3395f308edfacd16b5db27bed6c70977af
frank-Mwangi <kungofrank@gmail.com>
2023-02-06 12:24:29 -0800

    Defining child class rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module containing class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Defining the class attributes"""
+    pass
+
+    def area(self):
+        """Undefined method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate if parameter is integer"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Child of BaseGeometry, defines a rectangle"""
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
