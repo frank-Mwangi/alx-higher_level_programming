e6f4607840ca53a2b16a9c0025eb9970ffa331e0
frank-Mwangi <kungofrank@gmail.com>
2023-02-07 08:59:32 -0800

    Finding JSON equivalent of a string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that converts string to JSON
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """Finding JSON representation"""
+    return json.dumps(my_obj)
