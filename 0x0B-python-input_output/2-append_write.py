c4c54a7fd9465984ed8442cc3245d1e76c5de022
frank-Mwangi <kungofrank@gmail.com>
2023-02-07 08:20:28 -0800

    Appending strings onto a text file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+"""
+This module appends a string at the end
+of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """opening the file for writing"""
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
