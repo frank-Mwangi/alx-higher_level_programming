6f6ba9c36297906e88048233fb0c3b418d538109
frank-Mwangi <kungofrank@gmail.com>
2023-02-07 09:39:58 -0800

    Deserializing JSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """opening file and deserializing contents"""
-    with open(filename, "r", encoding="utf-8"):
-        json.load(f)
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
