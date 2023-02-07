f20e475f7dc98b0150ab5d4e44d7a721e5be660b
frank-Mwangi <kungofrank@gmail.com>
2023-02-07 11:35:34 -0800

    returns the dictionary description with simple data structures

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains a python class to JSON function
+"""
+
+
+def class_to_json(obj):
+    """Return dictionary representation of a  simple data structure"""
+    return obj.__dict___
