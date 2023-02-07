fd1dc3cf6b6dca07173ef3faaadc93d6867205b9
frank-Mwangi <kungofrank@gmail.com>
2023-02-07 12:28:57 -0800

    Pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that creates Pascal's triangle of integers
+"""
+
+
+def pascal_triangle(n):
+    """Create a Pascal's triangle of size n"""
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        temp = [1]
+        for i in range(len(tri) - 1):
+            temp.append(tri[i] + tri[i + 1])
+        temp.append(1)
+        triangles.append(temp)
+    return triangles
