c091aa3f4d131dd21ff2f66bc5132f3fd490b013
frank-Mwangi <kungofrank@gmail.com>
2023-02-07 09:38:18 -0800

    Deserializing JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module creates an object from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """opening file and deserializing contents"""
+    with open(filename, "r", encoding="utf-8"):
+        json.load(f)
