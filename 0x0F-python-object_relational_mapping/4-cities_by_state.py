32fd2e10df2a4dc04f45ab1d254e664285d917a4
frank-Mwangi <kungofrank@gmail.com>
2023-04-15 06:51:05 -0700

    List all cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,13 +12,11 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
     cur = db.cursor()
-    state = sys.argv[4]
     cur.execute("SELECT cities.id, cities.name, states.name\
             FROM cities INNER JOIN states ON\
-            cities.states_id=states.id")
+            cities.state_id=states.id")
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1] == state:
-            print(row)
+        print(row)
     cur.close()
     db.close()
