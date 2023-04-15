5954595b3c6abdb91e832804175ddfd25a16edbc
frank-Mwangi <kungofrank@gmail.com>
2023-04-15 07:21:36 -0700

    List all cities in given state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,17 +11,20 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
-    cur = db.cursor()
     state = sys.argv[4]
+    cur = db.cursor()
     cur.execute("SELECT cities.name\
         FROM cities JOIN states ON\
         cities.state_id=states.id\
-        WHERE states.name LIKE %s\
+        WHERE states.name= %s\
         ORDER BY cities.id", (state,))
     query_rows = cur.fetchall()
-    end = ""
+    i = 0
     for row in query_rows:
-        end += row[0] + ", "
-        print(end[0:-2:])
+        if i > 0:
+            print(", ", end="")
+        print(row[0], end="")
+        i += 1
+    print("")
     cur.close()
     db.close()
