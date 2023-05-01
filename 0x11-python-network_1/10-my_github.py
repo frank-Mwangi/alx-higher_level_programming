747437171679088924a130c7d71b32a1dbdeca7e
frank-Mwangi <kungofrank@gmail.com>
2023-05-01 17:58:42 +0300

    Using Github credentials as arguments, display Github id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,4 +9,12 @@ import requests
 
 
 if __name__ == "__main__":
-
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    cred = HTTPBasicAuth(username, password)
+    res = requests.get(url, auth=cred)
+    try:
+        print(res.json().get("id"))
+    except Exception:
+        print('None')
